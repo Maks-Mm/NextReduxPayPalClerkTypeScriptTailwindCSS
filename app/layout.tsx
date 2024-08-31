f4c42cdcf1b99de067bc59b02116e46883877d9b
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-08-31 19:28:14 +0200

    Background CodePen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+//import Style from "@/src/newFolder/Style";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,37 @@
-import { Button } from '@/components/ui/button';
-import React from 'react'
-
+import { Button } from "@/components/ui/button";
+import React from "react";
+import "./index.css";
 
 const HomePage = () => {
   return (
-    <div className='text-8xl text-red-400'>
-      HomePage
-      <Button>ShadCn</Button>
-    </div>
-  )
-}
+    <main className="app">
+      <div className="boxes">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className="text-8xl text-red-400">
+        HomePage
+        <Button>Button</Button>
+      </div>
+    </main>
+  );
+};
 
 export default HomePage;
+
+/*
+
+Werkzeuge https://ui.shadcn.com/docs/installation/next  
+https://nextjs.org/docs/getting-started/installation
+
+https://fonts.google.com/selection/embed (finden eine Antwort der Frage warum ich keine Fonts umzusetzen kann)
+
+https://codepen.io/sushantsharma20/pen/vwVMXR (Hintergrund mit einer Animation)
+
+
+https://www.youtube.com/watch?v=MG1Hc7VWnvc
+(12.00)time
+*/
