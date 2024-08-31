c075a9e9e71646e5cd4df9f5972a140676065f4d
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-08-31 17:19:16 +0200

    HomePage

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button';
+import React from 'react'
+
+
+const HomePage = () => {
+  return (
+    <div className='text-8xl text-red-400'>
+      HomePage
+      <Button>ShadCn</Button>
+    </div>
+  )
+}
+
+export default HomePage;
