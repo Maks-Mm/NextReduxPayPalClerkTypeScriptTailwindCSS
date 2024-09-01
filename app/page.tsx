b46ea05b7c6ba830212b9a79e6002415d5d01448
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-09-01 16:30:18 +0200

    Math fur ul und li von Quadraten

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,33 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 import "./index.css";
 
+const Box = () => {
+  const size = Math.round(Math.random() * 10) * 10;
+
+  const position = Math.round(Math.random() * 95);
+
+  return (
+    <li
+      style={{ left: `${position}%`, height: `${size}px`, width: `${size}px` }}
+    ></li>
+  );
+};
+
 const HomePage = () => {
   return (
     <main className="app">
       <div className="boxes">
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+          {[...Array(20)].map((_, index) => (
+            <Box key={index} />
+          ))}
         </ul>
       </div>
-      <div className="text-8xl text-red-400">
-        HomePage
+      <div className="text-8xl w-[150px] h-[150px]">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, quod
+        placeat, architecto perspiciatis rerum impedit odit blanditiis ea
+        inventore corrupti quasi dolorem cupiditate laborum facilis aspernatur
+        expedita sunt? A, ratione.
         <Button>Button</Button>
       </div>
     </main>
