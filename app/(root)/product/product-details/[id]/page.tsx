b351f765e17d7e0bd53c16e3999c3f536fd1ec69
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-09-11 12:50:13 +0200

    created a rating,React icon stars with Math.random and tailwind styling for reviews and prices
    //localhost den ich brauche http://localhost:3000/product/product-details/2 AdresPage

--- app/(root)/product/product-details/[id]/page.tsx
+++ app/(root)/product/product-details/[id]/page.tsx
@@ -6,6 +6,7 @@ import { getProductByCategory, getSingleProduct } from "@/Request/requests";
 import React from "react";
 import { Product } from "@/typing";
 import Image from "next/image";
+import { StarIcon } from "lucide-react";
 const ProductDetails = async ({ params }: { params: { id: string } }) => {
   const id = params.id;
 
@@ -25,11 +26,40 @@ const ProductDetails = async ({ params }: { params: { id: string } }) => {
       <div className="w-4/5 mx-auto grid grid-cols-1 lg:grid-cols-7 items-center gap-4">
         {/*Images */}
         <div className="col-span-3 mb-6 lg:mb-0">
-          <Image src={singleProduct.image} width={400} height={400} alt={singleProduct.title}/>
+          <Image
+            src={singleProduct.image}
+            width={400}
+            height={400}
+            alt={singleProduct.title}
+          />
         </div>
         {/*Content */}
         <div className="col-span-4">
-          <h1 className="lg:text-3xl text-2xl font-bold text-black">{singleProduct.title}</h1>
+          {/*Title */}
+          <h1 className="lg:text-3xl text-2xl font-bold text-black">
+            {singleProduct.title}
+          </h1>
+          {/*Ratings */}
+          <div className="mt-2 flex items-center space-x-2">
+            <div className="flex items-center">
+              {starArray.map((star) => {
+                return (
+                  <StarIcon
+                    key={Math.random() * 5000}
+                    size={20}
+                    fill="yellow"
+                    className="text-yellow-600"
+                  />
+                );
+              })}
+            </div>
+            <p className="text-base text-gray-700 font-semibold">
+             ( {singleProduct?.rating.count}Reviews)
+            </p>
+          </div>
+          {/*Line */}
+          <span className="w-1/4 h-[1.6px] bg-gray-400 rounded-lg block mt-4 opacity-20 mb-4"></span>
+          <h1 className="lg:text-6xl text-3xl md.text-4xl text-blue-950 font-bold">{singleProduct?.price.toFixed(2)}</h1>
         </div>
       </div>
     </div>
